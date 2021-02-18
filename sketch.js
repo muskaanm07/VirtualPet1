d3b87a1d2eed929251126999dbbf883eb0c7103f
muskaanm07 <75594700+muskaanm07@users.noreply.github.com>
2021-02-17 22:05:17 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,24 @@
+var dog, happyDog; 
+var database; 
+var foodS, foodStock;
+
+function preload()
+{
+	//load images here
+}
+
+function setup() {
+	createCanvas(800, 700);
+  
+}
+
+
+function draw() {  
+
+  drawSprites();
+  //add styles here
+
+}
+
+
+
